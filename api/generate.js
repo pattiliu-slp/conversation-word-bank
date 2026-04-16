7ef5736f2ad80d9157d601b00d8de6c3d1fcf960
pattiliu-slp <pattiliu@gmail.com>
2026-04-16 12:48:50 -0700

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,5 +1,3 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-
 module.exports = async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
@@ -21,6 +19,6 @@ module.exports = async function handler(req, res) {
     const data = await response.json();
     return res.status(response.status).json(data);
   } catch (error) {
-    return res.status(500).json({ error: "Failed to contact Anthropic API" });
+    return res.status(500).json({ error: error.message });
   }
 }
