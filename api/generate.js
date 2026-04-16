587c6f4e25971a183359edce2dc15c3f0f658ea2
pattiliu-slp <pattiliu@gmail.com>
2026-04-16 12:42:19 -0700

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,4 +1,6 @@
-export default async function handler(req, res) {
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+module.exports = async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
